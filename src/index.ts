39a169bac4e0d023b3124c4fee8e2e2aad0b5b87
Shaik849 <sanavullashaik0@gmail.com>
2025-04-02 12:54:46 +0530

    adding a route / to test

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import express, { Request, Response } from "express";
 const app = express();
 const PORT = 3000;
 
+app.get("/", async (req: Request, res: Response) : Promise<any> => {
+    return await res.json({ message: "hello world" });
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
